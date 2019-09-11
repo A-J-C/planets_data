fbdc4a58a80cf0e916d07e9539593131c41920f0
Alex C <3769586+A-J-C@users.noreply.github.com>
2019-09-11 19:16:21 +0100

    planet data object implementation, all tests passed

---
+++ src/main/java/com/portswigger/planets/Planet.java
@@ -0,0 +1,35 @@
+/*********************
+ * Author: Alex Craig
+ * Date: 10/09/2019
+ * Description: Stores data object for planet
+ *      Uses several public final variables defined below
+ *      For succinctness no getters or setters since public final
+ **********************/
+
+package com.portswigger.planets;
+
+public class Planet {
+
+    // name of planet stored as String
+    public final String name;
+    // number of moons stored as int (unrealistic to need a long for this)
+    public final int moons;
+    // weight of planet as a long as could get very largg
+    public final long weight;
+    // presence of water stored as boolean
+    public final boolean water;
+    // presence of stable atmosphere also stored as boolean
+    public final boolean atmosphere;
+    // presence of elements by atmoic number stored as byte array
+    // largest atomic number is 118 so this fits in +- 127 range easily
+    public final byte[] elements;
+
+    public Planet(String name, int moons, long weight, boolean water, boolean atmosphere, byte[] elements) {
+        this.name = name;
+        this.moons = moons;
+        this.weight = weight;
+        this.water = water;
+        this.atmosphere = atmosphere;
+        this.elements = elements;
+    }
+}
